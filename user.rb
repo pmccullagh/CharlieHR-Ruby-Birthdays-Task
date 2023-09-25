ec221602bf3c730ea212734f5fdc22f557f8aefa
Paul McCullagh <p.m.mccullagh@gmail.com>
2023-09-25 11:24:04 +0100

    First attempts.

--- user.rb
+++ user.rb
@@ -10,12 +10,20 @@ class User
 
   # Returns true if it is the user's birthday today
   def birthday?
-    # ...
+    if (@date_of_birth.day == Date.today.day) && (@date_of_birth.month == Date.today.month)
+      'true'
+    else
+      'false'
+    end
   end
 
   # Returns an Integer representing the user's current age
   def age
-    # ...
+    if @date_of_birth.month < Date.today.month || (@date_of_birth.month == Date.today.month && @date_of_birth.day <= Date.today.day)
+        Date.today.year - @date_of_birth.year
+    else
+      (Date.today.year - @date_of_birth.year) - 1
+    end
   end
 
   # Returns a Date object for the user's current upcoming birthday
@@ -23,6 +31,10 @@ class User
   # Note: if their birthday is today, their next birthday is next year.
   #
   def next_birthday
-    #...
+    if @date_of_birth.month < Date.today.month || (@date_of_birth.month == Date.today.month && @date_of_birth.day <= Date.today.day)
+      Date.new(Date.today.year, @date_of_birth.month, @date_of_birth.day).next_year
+    else
+      Date.new(Date.today.year, @date_of_birth.month, @date_of_birth.day)
+    end
   end
 end
